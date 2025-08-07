d145d5d5c52e7b4db682c42c0db097840477eb74
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-07 12:55:33 -0300

    feat: generates a link to get webToken

--- services/youtubeCredentials.go
+++ services/youtubeCredentials.go
@@ -1,3 +1,12 @@
+func GetWebTokenLink() string {
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	return authURL
+}
+
 func loadConfig() (*oauth2.Config, error) {
 	b, err := os.ReadFile("client_secret.json")
 	if err != nil {
