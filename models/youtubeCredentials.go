0ad782426c71d551a9bb9dde001cf5c85e782221
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-06 21:56:49 -0300

    feat: include OwnerUsername in YouTubeCredentials

--- models/youtubeCredentials.go
+++ models/youtubeCredentials.go
@@ -3,9 +3,10 @@ package models
 import "database/sql"
 
 type YouTubeCredentials struct {
-	AccessToken  string         `json:"access_token"`
-	TokenType    string         `json:"token_type"`
-	RefreshToken sql.NullString `json:"refresh_token"`
-	Expiry       sql.NullString `json:"expiry"`
-	ExpiresIn    sql.NullInt64  `json:"expires_in"`
+	OwnerUsername string         `json:"owner_username"`
+	AccessToken   string         `json:"access_token"`
+	TokenType     string         `json:"token_type"`
+	RefreshToken  sql.NullString `json:"refresh_token"`
+	Expiry        sql.NullString `json:"expiry"`
+	ExpiresIn     sql.NullInt64  `json:"expires_in"`
 }
