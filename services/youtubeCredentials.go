dc335f8f069e7e0d115e3ed3c65eb95b1570e5cb
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-07 12:55:04 -0300

    feat: load YouTube configs from client_secret.json

---
+++ services/youtubeCredentials.go
@@ -0,0 +1,11 @@
+func loadConfig() (*oauth2.Config, error) {
+	b, err := os.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.ConfigFromJSON(b, youtube.YoutubeReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	return config, nil
+}
