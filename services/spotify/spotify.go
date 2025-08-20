b43f8711d74665410e15423a736108d77eafff80
Samuel Lucas Matos <samuel.matos@wildlifestudios.com>
2025-08-19 23:59:21 -0300

    refactor: change REDIRECT_ROUTE implementation

--- services/spotify/spotify.go
+++ services/spotify/spotify.go
@@ -10,17 +10,27 @@ import (
 
 const (
 	AUTH_URL       = "https://accounts.spotify.com/authorize"
-	REDIRECT_ROUTE = "callback"
+	REDIRECT_ROUTE = "%s://%s:%s/callback"
 )
 
 var scopes = []string{"playlist-read-private", "playlist-modify-private", "playlist-modify-public"}
 
 func GetAuthURL() string {
-	redirectUri := fmt.Sprintf("%s://%s:%s/%s", configs.Cfg.FrontConfig.Protocol, configs.Cfg.FrontConfig.Host, configs.Cfg.FrontConfig.Port, REDIRECT_ROUTE)
+	protocol := configs.Cfg.FrontConfig.Protocol
+	host := configs.Cfg.FrontConfig.Host
+	port := configs.Cfg.FrontConfig.Port
+	redirectRoute := fmt.Sprintf(REDIRECT_ROUTE, protocol, host, port)
+
 	state := utils.GenerateRandomString(16)
 	formatedScopes := strings.Join(scopes, "%20")
 
-	queryString := fmt.Sprintf("client_id=%s&response_type=%s&redirect_uri=%s&state=%s&scope=%s", configs.Cfg.SpotifyConfig.ClientId, "code", redirectUri, state, formatedScopes)
+	queryString := fmt.Sprintf("client_id=%s&response_type=%s&redirect_uri=%s&state=%s&scope=%s",
+		configs.Cfg.SpotifyConfig.ClientId,
+		"code",
+		redirectRoute,
+		state,
+		formatedScopes,
+	)
 
 	authUrlWithParams := fmt.Sprintf("%s?%s", AUTH_URL, queryString)
 
