5ecbbd4f4c17a3c3f6d64eccd64d7287fcbca12d
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-21 13:23:39 -0300

    test: token generation test

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -20,3 +20,23 @@ func TestHash(t *testing.T) {
 		t.Errorf("expected no error when checking hashed password, got %v", err)
 	}
 }
+
+func TestToken(t *testing.T) {
+	token, err := utils.GenerateToken(32)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if len(token) != 32 {
+		t.Errorf("expected token length to be 32, got %d", len(token))
+	}
+	if token == "" {
+		t.Error("expected token to be non-empty")
+	}
+	otherToken, err := utils.GenerateToken(32)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if token == otherToken {
+		t.Error("expected tokens to be unique")
+	}
+}
