3b95583772ef842111455a6ae8ecb1aa7b8a9d87
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-21 13:23:26 -0300

    test: hash tests

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -5,3 +5,18 @@ import (
 
 	"github.com/DevOps-Group-D/YouToFy-API/utils"
 )
+
+func TestHash(t *testing.T) {
+	password := "testpassword"
+	hashedPassword, err := utils.HashPassword(password)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if hashedPassword == "" {
+		t.Error("expected hashed password to be non-empty")
+	}
+	err = utils.CheckHashedPassword(hashedPassword, password)
+	if err != nil {
+		t.Errorf("expected no error when checking hashed password, got %v", err)
+	}
+}
