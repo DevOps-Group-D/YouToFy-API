07ab43645313c2253bd31fe3bac806a7474a52e8
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-21 12:51:08 -0300

    test: get users from db

--- tests/account_test.go
+++ tests/account_test.go
@@ -26,3 +26,39 @@ func TestRegister(t *testing.T) {
 	}
 
 }
+
+func TestGetUser(t *testing.T) {
+	accountData := struct {
+		username string
+		Password string
+	}{
+		username: "testuser",
+		Password: "testpassword",
+	}
+	err := ServiceAcc.Register(accountData.username, accountData.Password)
+	if err != nil {
+		t.Fatalf("failed to register user: %v", err)
+	}
+	conn, err := database.Connect()
+	if err != nil {
+		t.Fatalf("failed to connect to database: %v", err)
+	}
+	defer conn.Close()
+	row := conn.QueryRow("SELECT * FROM account WHERE username = $1", accountData.username)
+	if row.Err() != nil {
+		t.Fatalf("failed to query user: %v", row.Err())
+	}
+	account := &models.Account{}
+	err = row.Scan(&account.Username, &account.Password, &account.SessionToken, &account.CsrfToken)
+	if err != nil {
+		t.Fatalf("failed to scan user: %v", err)
+	}
+	hashedPassword, err := utils.HashPassword(accountData.username)
+	if err != nil {
+		t.Errorf("error hashing password")
+	}
+	if account.Username != hashedPassword {
+		t.Errorf("expected username %s, got %s", accountData.username, account.Username)
+	}
+
+}
