56021a04504142c13f632d3a54df0bbbe044f7a4
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-08-21 12:49:44 -0300

    test: account test file

---
+++ tests/account_test.go
@@ -0,0 +1,14 @@
+package tests
+
+import (
+	// error package in erros/account.go
+	"errors"
+	"testing"
+
+	"github.com/DevOps-Group-D/YouToFy-API/database"
+	yterrors "github.com/DevOps-Group-D/YouToFy-API/errors"
+	"github.com/DevOps-Group-D/YouToFy-API/models"
+	ServiceAcc "github.com/DevOps-Group-D/YouToFy-API/services"
+	"github.com/DevOps-Group-D/YouToFy-API/utils"
+)
+
