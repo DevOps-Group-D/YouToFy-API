000e545feee7109c2ead1c40baddc420eeb78a7b
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-09-03 19:46:58 -0300

    feat: implement save request

--- controllers/youtube/account.go
+++ controllers/youtube/account.go
@@ -1,7 +1,7 @@
 package youtube
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	youtubeService "github.com/DevOps-Group-D/YouToFy-API/services/youtube"
@@ -9,16 +9,28 @@ import (
 
 func (p YoutubeProvider) Login(w http.ResponseWriter, r *http.Request) {
 	authUrl := youtubeService.GetAuthURL()
-	fmt.Println(authUrl)
 	http.Redirect(w, r, authUrl, http.StatusTemporaryRedirect)
-	response := "Youtube authorization route called"
+	response := `{"authUrl": "` + authUrl + `"}`
 
 	w.Header().Add("Content-Type", "application/json")
 	w.Write([]byte(response))
 	w.WriteHeader(302)
-	println("reached-end")
 }
 
-// func (p YoutubeProvider) save(w http.ResponseWriter, r *http.Request) {
-
-// }
+func (p YoutubeProvider) Save(w http.ResponseWriter, r *http.Request) {
+	// code := r.Header.Get("code")
+	code := r.URL.Query().Get("code")
+	authToken, error := youtubeService.GetWebTokenFromCode(code)
+	if error != nil {
+		http.Error(w, "error retrieving token from code: "+error.Error(), http.StatusInternalServerError)
+		return
+	}
+	jsonResponse, err := json.Marshal(authToken)
+	if err != nil {
+		http.Error(w, "error marshalling token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(jsonResponse)
+	w.WriteHeader(http.StatusOK)
+}
